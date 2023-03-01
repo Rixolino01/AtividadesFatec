0e5b053605103d12c98258a558d54f49cacb6eaa
Richardson Trizzino <89614250+Rixolino01@users.noreply.github.com>
2023-03-01 13:16:29 -0300

    transormando em inteiro

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 const int a = prompt("Informe o salario a ser alterado: ");
 const int b = prompt("informe o valor em porcentagem a ser corrigido: ");
 
-let salário = (a*(b/100))+a;
+let int salário = (a*(b/100))+a;
 
 document.write(salário);
