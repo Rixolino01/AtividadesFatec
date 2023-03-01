a7aef6ac7b89999211cbb1b502d0d023a9d7033f
Richardson Trizzino <89614250+Rixolino01@users.noreply.github.com>
2023-03-01 12:48:57 -0300

    Atualização Script

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 const a = prompt("Informe o salario a ser alterado: ");
-/*const b = prompt("Insira o valor percentual do aumento: ")
+const b = prompt("Insira o valor percentual do aumento: ")
 
 let salário = (a*(b/100))+a
 
-document.write = salário*/
+document.write = salário
