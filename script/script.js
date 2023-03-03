6b9370b0420814d3c4c117419f8722dec866cc9d
Richardson Trizzino <89614250+Rixolino01@users.noreply.github.com>
2023-03-02 21:21:02 -0300

    codigo ajuste

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
-const int a = prompt("Informe o salario a ser alterado: ");
-const int b = prompt("informe o valor em porcentagem a ser corrigido: ");
+const a = Number(prompt("Informe o salario a ser alterado: "));
+const b = Number(prompt("informe o valor em porcentagem a ser corrigido: "));
 
-let int salário = (a*(b/100))+a;
+let salário = (a*(b/100))+a;
 
 document.write(salário);
