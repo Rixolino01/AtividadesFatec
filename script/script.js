47189425e664d462df24859ce17ad32d5ee39259
Richardson Trizzino <89614250+Rixolino01@users.noreply.github.com>
2023-03-03 06:55:33 -0300

    Adjust JS

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 const a = Number(prompt("Informe o salario a ser alterado: "));
 const b = Number(prompt("informe o valor em porcentagem a ser corrigido: "));
 
-let salário = (a*(b/100))+a;
+let  salario =Number (a*(b/100))+a;
 
 document.write(salário);
